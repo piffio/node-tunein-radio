fa15936787bcb3e76db6e7cfef7926ea3405ef3a
Sergio Visinoni <piffio@piffio.org>
2019-06-15 15:15:15 +0200

    Refactor test again

--- test/test.js
+++ test/test.js
@@ -78,12 +78,11 @@ describe('#tuneinRadio', function () {
       expect(stationList.text).to.not.be.undefined;
       if (stationsList.children !== undefined) {
         expect(stationList.children).to.be.an('array');
+        let station = stationList.children[0];
+        expect(station.element).to.equal("outline");
+        expect(station.type).to.equal("audio");
+        expect(station.item).to.equal("station");
       }
-
-      let station = stationList.children[0];
-      expect(station.element).to.equal("outline");
-      expect(station.type).to.equal("audio");
-      expect(station.item).to.equal("station");
     });
   });
 
